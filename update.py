43c364e86234a871f392bf10e62531988992748a
Lulzx <wiserishabh@gmail.com>
2020-04-20 15:05:23 +0530

    Create update.py

---
+++ update.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+import os
+
+
+HEADER="""# TIL
+
+> 📝 Today I Learned
+
+
+A collection of stuff that I learn every day.
+
+---
+
+"""
+
+
+def main():
+    content = ""
+    content += HEADER
+
+    for root, dirs, files in os.walk("."):
+        dirs.sort()
+        if root == '.':
+            for dir in ('.git', '.github'):
+                try:
+                    dirs.remove(dir)
+                except ValueError:
+                    pass
+            continue
+
+        category = os.path.basename(root)
+
+        content += "### {}\n\n".format(category)
+
+        for file in files:
+            name = os.path.basename(file).split('.')[0]
+            name = " ".join(word.capitalize() for word in name.split('-'))
+            content += "- [{}]({})\n".format(name, os.path.join(category, file))
+        content += "\n"
+
+    with open("README.md", "w") as fd:
+        fd.write(content)
+
+
+if __name__ == "__main__":
+    main()
